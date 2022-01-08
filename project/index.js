d3473fe2e910951c67eb3c36f313c30db8a1a017
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2022-01-08 18:27:25 +0530

    static and dynamic content

--- project/index.js
+++ project/index.js
@@ -3,17 +3,22 @@ var app = express()
 
 var handlebars = require("express3-handlebars").create({defaultLayout : 'main'});
 
+var dynamicString = "This is written dynamically!!!";
+
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT || 3000);
+
+app.use(express.static("public"));
+
 
 app.get("/",(req,res)=>{
     res.render('home');
 });
 
 app.get("/about",(req,res)=>{
-    res.render('about');
+    res.render('about',{dynamicContent : dynamicString});
 });
 
 app.use((req,res)=>{
