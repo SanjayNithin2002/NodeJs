b5ea16e40cce2c90d82e5245bc46d49161840b12
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2022-01-13 23:51:33 +0530

    rolling back

--- project/index.js
+++ project/index.js
@@ -1,39 +1,40 @@
 var express = require("express")
 var app = express()
 
-var handlebars = require("express3-handlebars").create({defaultLayout : 'main'});
+var handlebars = require("express3-handlebars").create({ defaultLayout: 'main' });
 
 var dynamicString = "This is written dynamically!!!";
 
-app.engine('handlebars',handlebars.engine);
-app.set('view engine','handlebars');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-app.set('port',process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static("public"));
 
 
 //internal testing code
 //global testing
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.showTests = app.get("env") !== 'production' && req.query.test === '1';
     next();
 });
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('home');
-});
+});terminal
 
 
 //page dependent testing
-app.get("/about",(req,res)=>{
-    res.render('about',{
-        dynamicContent : dynamicString,
+app.get("/about", (req, res) => {
+    res.render('about', {
+        dynamicContent: dynamicString,
         pageTestScript: '/Contact'
     });
 });
+
 
 /*
 dependency files are added in public/vendor directory.
@@ -48,11 +49,11 @@ if it is contact it is successful
 or it a failure.
 */
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404);
     res.render('notfound');
 });
 
-app.listen(app.get('port'),()=>{
+app.listen(app.get('port'), () => {
     console.log("logged in");
 });
