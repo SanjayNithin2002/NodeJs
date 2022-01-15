dfc5b08c50ddfd150ae69daa866fb1def3fd95c0
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2022-01-16 02:38:33 +0530

    templating is tried out

--- project/index.js
+++ project/index.js
@@ -1,14 +1,7 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 
 var handlebars = require("express3-handlebars").create({ defaultLayout: 'main' });
-
-var dynamicString = [
-    {firstname : "Sanjay" , lastname : "Nithin"},
-    {firstname : "Ajay" , lastname : "Devgan"},
-    {firstname : "Ishani" , lastname : "Bhavya"},
-    {firstname : "Jawahar" , lastname : "Prayush"}
-];
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars'); //choosing handlesbars as viewengine 
@@ -20,23 +13,32 @@ app.use(express.static("public"));
 
 //internal testing code
 //global testing
-app.use((req, res, next) => {
+app.use(function(req, res, next){
     res.locals.showTests = app.get("env") !== 'production' && req.query.test === '1';
     next();
 });
 
 
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res){
     res.render('home');
 });
 
 
 //page dependent testing
-app.get("/about", (req, res) => {
-    res.render('about', {
-        dynamicContent: dynamicString,
-        pageTestScript: '/Contact'
+app.get("/about", function(req, res){
+    res.render('about',{
+        author : {
+            username : "SanjayNithin2002",
+            repository : "NodeJS"
+        },
+        dynamicString: [
+            { firstname: "Sanjay", lastname: "Nithin" },
+            { firstname: "Ajay", lastname: "Devgan" },
+            { firstname: "Ishani", lastname: "Bhavya" },
+            { firstname: "Jawahar", lastname: "Prayush" }
+        ],
+        id : false // change it to true and access {{if ../id}}
     });
 });
 
@@ -63,12 +65,12 @@ app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
 
-app.use((req, res) => {
+app.use(function(req, res){
     res.status(404);
     res.render('notfound');
 });
 
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'),function(){
     console.log("logged in");
 });
