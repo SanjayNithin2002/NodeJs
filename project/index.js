ddf2c6da66339e8c62609053edb36f4b9335c9fa
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2022-01-16 03:42:13 +0530

    partials added

--- project/index.js
+++ project/index.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var app = express();
 
-var handlebars = require("express3-handlebars").create({ defaultLayout: 'main' });
+var handlebars = require("express3-handlebars").create({
+    defaultLayout: 'main', helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars'); //choosing handlesbars as viewengine 
-app.set('view cache',true);
+app.set('view cache', true);
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static("public"));
@@ -13,24 +21,32 @@ app.use(express.static("public"));
 
 //internal testing code
 //global testing
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.showTests = app.get("env") !== 'production' && req.query.test === '1';
     next();
 });
 
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.data = [
+        { name: "Sanjay", age: '19' },
+        { name: "Nithin", age: '20' },
+        { name: "Ajay", age: 23 }
+    ];
+    next();
+});
 
-
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render('home');
 });
 
 
 //page dependent testing
-app.get("/about", function(req, res){
-    res.render('about',{
-        author : {
-            username : "SanjayNithin2002",
-            repository : "NodeJS"
+app.get("/about", function (req, res) {
+    res.render('about', {
+        author: {
+            username: "SanjayNithin2002",
+            repository: "NodeJS"
         },
         dynamicString: [
             { firstname: "Sanjay", lastname: "Nithin" },
@@ -38,14 +54,14 @@ app.get("/about", function(req, res){
             { firstname: "Ishani", lastname: "Bhavya" },
             { firstname: "Jawahar", lastname: "Prayush" }
         ],
-        id : false // change it to true and access {{if ../id}}
+        id: false // change it to true and access {{if ../id}}
     });
 });
 
-app.get("/custom-layout",function(req,res){
-    res.render('customLayoutTest',{
-        layout : 'custom',
-        copyrightYear : '2022'
+app.get("/custom-layout", function (req, res) {
+    res.render('customLayoutTest', {
+        layout: 'custom',
+        copyrightYear: '2022'
     });
 });
 /*
@@ -70,12 +86,12 @@ app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
 });
 
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404);
     res.render('notfound');
 });
 
 
-app.listen(app.get('port'),function(){
+app.listen(app.get('port'), function () {
     console.log("logged in");
 });
