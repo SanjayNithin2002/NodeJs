bb64948af1d225f95ca5a1e28059d7f64d2b2bb0
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2022-01-29 22:37:28 +0530

    all cmmits

---
+++ project/credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+    cookieSecret : "this is a cookie",
+    gmail : {
+        user : "sanjaynithin1910@gmail.com",
+        password : "sanjay&19/10/2002"
+    },
+    mongoDb : {
+        connectString : "mongodb+srv://sanjay:sanjay2002@cluster0.w9ghb.mongodb.net/firstDatabase?retryWrites=true&w=majority"
+    }
+};
